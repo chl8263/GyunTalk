f02f3331df3efcdce091ce1d0c2f98872e556cef
gyun_home <chl8263@maver.com>
2018-06-26 16:18:12 +0900

    Initial commit

---
+++ app/src/main/java/com/example/gyun_home/gyuntalk/Main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.gyun_home.gyuntalk.Main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.gyun_home.gyuntalk.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
